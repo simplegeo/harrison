c21ed7caf39bf61f5f8324f424eb5b970deb98af
Seth & Wade <boulder@simplegeo.com>
2010-07-08 16:07:54 -0600

    Move buffered tasks into the pending set

--- test/fresnel.test.js
+++ test/fresnel.test.js
@@ -8,39 +8,56 @@ process.addListener('uncaughtException', function(err) {
     console.log("Caught exception: " + err);
 });
 
+function randomTask() {
+    return {
+        "class": "class:" + new Date().getTime(), // TODO random string helper
+        "args": []
+    };
+}
+
 module.exports = {
     'should be an EventEmitter': function(assert) {
         var fresnel = new Fresnel();
         assert.ok(fresnel instanceof EventEmitter);
     },
-    'should buffer tasks into a local queue': function(assert, beforeExit) {
+     'should buffer tasks into a local queue': function(assert, beforeExit) {
         var fresnel = new Fresnel('test:' + new Date().getTime());
 
         // create some tasks in Redis
-        var task = {
-            "class": "Sample",
-            "args": []
-        }
+        var task = randomTask();
 
         fresnel.createTask(task, function() {
             fresnel.bufferTasks(function() {
-                fresnel.shutdown();
+                fresnel.getUnbufferedQueueLength(function(length) {
+                    try {
+                        assert.equal(0, length);
+                    } finally {
+                        fresnel.shutdown();
+                    }
+                });
             });
         });
 
         beforeExit(function() {
             assert.equal(task.toString(), fresnel.BUFFERED_TASKS[0].toString());
-
-            fresnel.getUnbufferedQueueLength(function(length) {
-                assert.equal(0, length);
-            });
-
-            fresnel.shutdown();
         });
+     },
+    'should mark buffered tasks as pending': function(assert, beforeExit) {
+        var fresnel = new Fresnel('test:' + new Date().getTime());
 
-    },
-    'should mark buffered tasks as pending': function(assert) {
-
+        console.log("creating tasks");
+        fresnel.createTask(randomTask(), function() {
+            console.log("buffering tasks");
+            fresnel.bufferTasks(function() {
+                fresnel.getPendingCount(function(count) {
+                    try {
+                        assert.equal(1, count);
+                    } finally {
+                        fresnel.shutdown();
+                    }
+                });
+            });
+        });
     },
     'create task adds shaw for uniqueness': function(assert) {
     }
