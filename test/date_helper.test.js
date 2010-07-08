e71640cf6f94990c32bfb15434e1ff4bf7ca3dab
Seth Fitzsimmons <seth@simplegeo.com>
2010-07-08 12:29:46 -0600

    ISO date formatting.

---
+++ test/date_helper.test.js
@@ -0,0 +1,14 @@
+require('date_helper');
+
+module.exports = {
+    'test date formatting': function(assert) {
+        var date = new Date(1278612143347);
+        assert.equal('2010-07-08T18:02:23.347Z', date.toISOString());
+    },
+    'test date parsing': function(assert) {
+        assert.equal(1278612143347, Date.parse('2010-07-08T18:02:23.347Z'));
+    },
+    'test non-UTC date parsing': function(assert) {
+        assert.equal(1278612143347, Date.parse('2010-07-08T12:02:23.347-600'));
+    }
+}
