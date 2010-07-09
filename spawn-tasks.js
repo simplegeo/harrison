d85fc8ca288e05072dcb3645141ebdbc3f225b98
Seth Fitzsimmons <seth@simplegeo.com>
2010-07-09 14:42:47 -0600

    Minor bugfixes and interval tracking

--- spawn-tasks.js
+++ spawn-tasks.js
@@ -1,15 +1,33 @@
 var redis = require('redis-client'),
     Fresnel = require('fresnel').Fresnel;
 
-redis.debugMode = true;
+redis.debugMode = false;
 
 var fresnel = new Fresnel();
 
-var task = {
-    "class": "echo",
-    "args": []
+function randomTask() {
+    return {
+        "class": randomString(),
+        "args": []
+    };
 }
-fresnel.createTask(task, function() {
-    fresnel.shutdown();
-});
+
+var CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+function randomString(length) {
+    length = length || 16;
+    var text = "";
+
+    for (var i=0; i < length; i++) {
+        text += CHARACTERS.charAt(Math.floor(Math.random() * CHARACTERS.length));
+    }
+
+    return text;
+}
+
+for (var i = 0; i < 10; i++) {
+    fresnel.createTask(randomTask(), function() {
+        fresnel.shutdown();
+    });
+}
 
