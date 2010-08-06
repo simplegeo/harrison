62253bb2012221aa0c5b6082b96cb3ad7ffe1ec9
Seth Fitzsimmons <seth@simplegeo.com>
2010-08-06 17:29:55 -0600

    Migrate tasks from the reservoir, handle errors better

--- fresnel.js
+++ fresnel.js
@@ -1,3 +1,4 @@
+var sys = require('sys');
 var Fresnel = require('fresnel').Fresnel;
 
 var fresnel = new Fresnel();
@@ -9,8 +10,24 @@ fresnel.WORKER_MAP = {
     "*": "http://localhost:8081/"
 };
 
+fresnel.addListener('task-completed', function(task, response, time) {
+    console.log(task.id + " completed in " + time + "ms.");
+});
+
+fresnel.addListener('task-error', function(task, error, time) {
+    console.log("task " + task.id + " errored: " + error);
+});
+
+fresnel.addListener('task-failed', function(task, error, time) {
+    console.log("task " + task.id + " failed: " + error);
+});
+
 fresnel.getUnbufferedQueueLength(function(length) {
     console.log("Unbuffered tasks: " + length);
+});
+
+fresnel.getReservoirSize(function(length) {
+    console.log("Reservoir size: " + length);
 });
 
 fresnel.processTasks();
