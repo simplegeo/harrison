1eb9601ccd9ad0cf945a12775a012feb86f7d8a3
Seth Fitzsimmons <seth@simplegeo.com>
2010-08-06 14:34:22 -0600

    added the worker map

--- fresnel.js
+++ fresnel.js
@@ -2,6 +2,13 @@ var Fresnel = require('fresnel').Fresnel;
 
 var fresnel = new Fresnel();
 
+fresnel.WORKER_MAP = {
+    "Class": "http://localhost:8080/jobs/sample",
+    "Backfill": "http://localhost:8080/jobs/backfill",
+    "Load": "http://localhost:8081/",
+    "*": "http://localhost:8081/"
+};
+
 fresnel.getUnbufferedQueueLength(function(length) {
     console.log("Unbuffered tasks: " + length);
 });
